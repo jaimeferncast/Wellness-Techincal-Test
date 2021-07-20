516fa1d97fab253268e8157b837031b3f4d6692a
Jaime <jaimeferncast@gmail.com>
2021-07-20 16:56:31 +0200

    setup index page and general styles

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -1,15 +1,12 @@
 import { Switch, Route } from "react-router-dom"
 
 import Index from "../pages/IndexPage/IndexPage"
-import PhoneList from "../pages/Phones/PhoneList"
-import PhonePage from "../pages/Phone-details/PhonePage"
 
 const Routes = () => {
     return (
         <Switch>
             <Route path="/" exact render={() => <Index />} />
-            <Route path="/teléfonos" exact render={() => <PhoneList />} />
-            <Route path="/teléfonos/:id" render={props => <PhonePage {...props} />} />
+            <Route path="/consumos" exact render={() => <Index />} />
         </Switch>
     )
 }
